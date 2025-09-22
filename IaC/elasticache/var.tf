ff481856f5b71c7b8c226e76c5ee9582b961a386
seungwoo1124 <seungwoo1124@kookmin.ac.kr>
2025-09-22 20:50:35 +0900

    Elasticache 배치 위치 data subnet 으로 변경

--- IaC/elasticache/var.tf
+++ IaC/elasticache/var.tf
@@ -1,3 +1,4 @@
 variable "prefix" { type = string }
 variable "elasticache_security_group_id" { type = string }
 variable "public_subnet_ids" { type = list(string) }
+variable "data_subnet_ids" { type = list(string) }
